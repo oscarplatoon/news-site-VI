936c60c1b06e87e7683f60117cd87dd9f3993455
Won Yang <won@gatech.edu>
2021-07-24 22:10:08 -0500

    class based solution

--- src/pages/AddArticlePage.js
+++ src/pages/AddArticlePage.js
@@ -1,61 +1,69 @@
 import React, { Component } from 'react';
 import { addArticle } from '../api/ArticlesAPI';
 import { Redirect } from 'react-router-dom';
-import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import UserContext from '../contexts/UserContext';
+
 
 class AddArticlePage extends Component {
-  state = {
-    redirect: false
-  }
-
-  handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const articleObject = {
-      title: event.target.elements[0].value,
-      byline: event.target.elements[1].value,
-      abstract: event.target.elements[2].value
+    state = {
+        redirect: false
     }
 
-    try {
-      const response = await addArticle(articleObject);
-      if (response.status === 200) {
-        // redirect the user back to Home Page upon successful POST
-        this.setState({ redirect: true });
-      } else {
-        const jsonData = await response.json();
-        alert(jsonData.error.message);
-      }
-    } catch (err) {
-      console.error('error occurred posting article: ', err);
-    }
-  }
+    handleFormSubmit = async (event, userContext) => {
+        event.preventDefault();
+        const articleObject = {
+            title: event.target.elements[0].value,
+            byline: event.target.elements[1].value,
+            abstract: event.target.elements[2].value
+        }
 
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to='/' />
+        try {
+            const response = await addArticle(articleObject, userContext.user.id);
+            if (response.status === 200) {
+                // redirect the user back to Home Page upon successful POST
+                this.setState({ redirect: true });
+            } else {
+                const jsonData = await response.json();
+                alert(jsonData.error.message);
+            }
+        } catch (err) {
+            console.error('error occurred posting article: ', err);
+        }
     }
 
-    return (
-      <div style={{ padding: '20px' }}>
-        <h3> Add an Article </h3>
-        <Form onSubmit={this.handleFormSubmit}>
-          <FormGroup>
-            <Label for="title">Title</Label>
-            <Input type="text" name="title" id="title" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="byline">Byline</Label>
-            <Input type="text" name="byline" id="byline" />
-          </FormGroup>
-          <FormGroup>
-            <Label for="abstract">Abstract</Label>
-            <Input type="textarea" name="abstract" id="abstract" />
-          </FormGroup>
-          <Button>Submit</Button>
-        </Form>
-      </div>
-    )
-  }
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to='/' />
+        }
+
+        return (
+            <div style={{ padding: '20px' }}>
+                <h3> Add an Article </h3>
+                <UserContext.Consumer>
+                    {userContext => (
+                        (userContext.user) ? (
+                            <Form onSubmit={(event) => this.handleFormSubmit(event, userContext)}>
+                                <FormGroup>
+                                    <Label for="title">Title</Label>
+                                    <Input type="text" name="title" id="title" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="byline">Byline</Label>
+                                    <Input type="text" name="byline" id="byline" />
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="abstract">Abstract</Label>
+                                    <Input type="textarea" name="abstract" id="abstract" />
+                                </FormGroup>
+                                <Button>Submit</Button>
+                            </Form>
+                        ) : <Redirect to='/login' />
+                    )}
+                </UserContext.Consumer>
+            </div>
+        )
+    }
 }
 
 export default AddArticlePage;
