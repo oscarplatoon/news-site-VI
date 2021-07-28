7d5b5a58683bd0315f087f3c12b351ac65c62198
tomprete <prete316@gmail.com>
2021-07-28 11:22:13 -0500

    instructor solution

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -1,44 +1,68 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, NavItem } from 'reactstrap';
-import navItems from '../../config/Sections.json';
+import React, { Component, useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Button, Form, FormControl } from 'react-bootstrap'
+import navItems from '../../config/Sections'
+import { withRouter } from "react-router";
+import UserContext from '../../contexts/UserContext';
 
-class AppNav extends Component {
-  render() {
-    return (
-      <Navbar color="light">
-        {navItems.map((navItem) =>
-          <NavItem>
-            <Link to={`/sections/${navItem.value}`} >
-              { navItem.label }
-            </Link>
-          </NavItem>
-        )}
-        <NavItem>
-          <Link to="/add-article">Add an Article</Link>
-        </NavItem>
-      </Navbar>
-    )
+// Classed Based Components
+// class AppNav extends Component {
+//   render() {
+//     const {navItems, handleNavClick} = this.props
+
+//     const createNavItems = () => {
+//       return navItems.map((navItem, i) => {
+//         return <a key={i} href='#' onClick={() => handleNavClick(navItem.value)}>{navItem.label}</a>
+//       })
+//     }
+//     return (
+//       <nav>
+//       {
+//         createNavItems()
+//       }
+//       </nav>
+//     )
+//   }
+// }
+
+// --- Functional Component
+const AppNav = (props) => {
+  const { handleSearch } = props;
+  const userContext = useContext(UserContext);
+  const { user } = userContext;
+
+
+  const createNavItems = () => {
+    return navItems.map((navItem, idx) => {
+      return <Nav.Link key={idx} onClick={() => props.history.push(`/sections/${navItem.value}`)}>{navItem.label}</Nav.Link>
+    })
   }
+  console.log(userContext)
+  console.log(props)
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand onClick={() => props.history.push('/')} className='m-2'>News Site</Navbar.Brand>
+      <Nav className="me-auto">
+        {
+          createNavItems()
+        }
+        {
+          user
+            ?
+            <div>
+              <Button href='/logout' variant='outline-danger' className='mx-2'>Logout</Button>
+              <Button onClick={() => props.history.push('/add-article')} variant='outline-secondary' className='mx-2'>Add Article</Button>
+            </div>
+            :
+            <Button href='/login' variant='outline-primary' className='mx-2'>Login</Button>
+        }
+      </Nav>
+      <Form className='d-flex' onSubmit={handleSearch}>
+        <FormControl type="search" placeholder="Search" className="mr-2" name='search' />
+        <Button type='submit' variant="outline-success">Search</Button>
+      </Form>
+    </Navbar>
+  )
 }
 
-export default AppNav;
-
-
-// Functional solution:
-// function AppNav() {
-//   return (
-//     <Navbar color="light">
-//       {navItems.map((navItem) =>
-//         <NavItem>
-//           <Link to={`/sections/${navItem.value}`} >
-//             {navItem.label}
-//           </Link>
-//         </NavItem>
-//       )}
-//       <NavItem>
-//         <Link to="/add-article">Add an Article</Link>
-//       </NavItem>
-//     </Navbar>
-//   );
-// }
+export default withRouter(AppNav);
--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import ArticleTeaser from '../ArticleTeaser/ArticleTeaser.js';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import ArticleTeaser from '../ArticleTeaser/ArticleTeaser';
+import {ListGroup} from 'react-bootstrap'
+import moment from 'moment';
 
 class ArticleList extends Component {
+
+  getAllTeasers = (articles) => {
+    return articles.map((article, id) => {
+      return <ListGroup.Item key={id} className='shadow p-3 mb-5 bg-white rounded border-0 m-2'>
+        <ArticleTeaser
+          id={id+1}
+          created_date={moment(article.created_date).format("MM-DD-YYYY")}
+          title={article.title}
+          handleTitleClick={this.props.handleTitleClick}
+          />
+      </ListGroup.Item>
+    })
+  }
+
   render() {
-    const { articles } = this.props;
     return (
-      <ListGroup>
-        { articles.map((article, index) => (
-          <ListGroupItem>
-            <ArticleTeaser { ...article } id={ index + 1 } />
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <div>
+            {
+        this.getAllTeasers(this.props.articles)
+      }
+      </div>
     );
   }
 }
 
 export default ArticleList;
-
-
-// Functional solution:
-// function ArticleList({ articles }) {
-//   return (
-//     <ListGroup>
-//       {articles.map((article, index) => (
-//         <ListGroupItem>
-//           <ArticleTeaser {...article} id={ index + 1 } />
-//         </ListGroupItem>
-//       ))}
-//     </ListGroup>
-//   );
-// }
